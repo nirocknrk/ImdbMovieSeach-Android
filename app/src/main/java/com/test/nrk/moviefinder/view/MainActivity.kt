d566840466bb2d0d350d76f8e0f2bd23594e0412
Niroshan <niroshan_chandra_wijaya_kumar_from.scientec@stb.gov.sg>
2025-02-18 02:02:47 +0800

    Linked ViewModel, Repository & ApiService class

--- app/src/main/java/com/test/nrk/moviefinder/view/MainActivity.kt
+++ app/src/main/java/com/test/nrk/moviefinder/view/MainActivity.kt
@@ -1,60 +1,46 @@
 package com.test.nrk.moviefinder.view
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import com.test.nrk.moviefinder.R
-import com.test.nrk.moviefinder.databinding.ActivityMainBinding
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.test.nrk.moviefinder.viewmodel.MovieViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
+
+    private val viewModel: MovieViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent {
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+            MaterialTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                ) {
 
-        setSupportActionBar(binding.toolbar)
+                    val moviesList by viewModel.moviesList.collectAsStateWithLifecycle()
+                    val searchProgress by viewModel.searchProgress.collectAsStateWithLifecycle()
+                    val errorMessage by viewModel.errorMessage.collectAsStateWithLifecycle()
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .setAnchorView(R.id.fab).show()
+                    CompositionLocalProvider {
+                        MovieScreen(moviesList, searchProgress,errorMessage) { viewModel.search(it) }
+                    }
+                }
+            }
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
-    }
 }
